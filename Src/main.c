ab4b7542e6d39cac098749837d8e0c43944e92da
Andreas Cnaus <cnaus.andreas@gmail.com>
2025-10-21 18:32:11 +0200

    [miscellaneous]
    vs-code settings and main adapred to the new implementation

--- Src/main.c
+++ Src/main.c
@@ -1,5 +1,7 @@
 #include "uart.h"
 #include "my_stdio.h"
+#include "systick.h"
+#include "sim7600e.h"
 
 #include <stdint.h>
 #include <stdio.h>
@@ -7,6 +9,13 @@
 
 int main(void)
 { 
+    const char *pin = "4949";
+    const char *url = "test_url";
+    uint8_t debug = 1;
+
+    // Initialize system tick 
+    systick_init();
+
     // Initialize  UART1 to communicate with SIM7600E-Module
     uart1_init();
 
@@ -15,11 +24,14 @@ int main(void)
     
     // Initialize stdio to use printf correctly 
     stdio_init();
+
+    // Initialize SIM7600E-Module 
+    sim7600e_init(pin, url, debug);
     
     /* Loop forever */
     while (1)
     {
-        printf("Hello from STM32F407_tracker_board firmware!\r\n");
+        
     }
 }
 
